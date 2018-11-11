aa40c1f20dbaaf79e82a508baa3e1a2f59493325
George Svarovsky <gsvarovsky@gmail.com>
2018-11-11 13:32:50 +0000

    Iteration to prevent dirty overlaps

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import runpy
 import sys
+import math
 
 import pygame
 from pygame.locals import *
@@ -16,17 +17,25 @@ def main():
     while not has_quit():
         all_group.clear(screen, background)
 
+        # Apply velocity
         for sprite in all_group:
-            # Apply velocity
             sprite.rect.move_ip(sprite.dx, sprite.dy)
 
+        # Resolve collisions with the walls
         for sprite in all_group:
-            # Resolve collisions with the walls - always wins
-            if resolve_wall_collision(sprite):
-                # Resolve collisions with other sprites
-                for collided in pygame.sprite.spritecollide(sprite, all_group, False):
-                    resolve_collision(sprite, collided)
+            resolve_wall_collision(sprite)
 
+        # Resolve collisions with other sprites
+        dirty_group = all_group.copy()
+        while dirty_group:
+            sprite = dirty_group.sprites()[0]
+            dirty_group.remove(sprite)
+            for collided in pygame.sprite.spritecollide(sprite, all_group, False):
+                if sprite is not collided:  # Do not include self
+                    resolve_collision(sprite, collided)
+                    dirty_group.add(sprite, collided)
+
+        for sprite in all_group:
             # Apply wind resistance
             sprite.dx = sprite.dx * AIR_RESISTANCE
             sprite.dy = sprite.dy * AIR_RESISTANCE
@@ -43,33 +52,29 @@ def main():
 
 def resolve_wall_collision(sprite):
     inscreen = sprite.rect.clamp(SCREEN_RECT)
-    if inscreen.center != sprite.rect.center:
-        if inscreen.centerx != sprite.rect.centerx:
-            sprite.dx = -sprite.dx * sprite.bounce
-        if inscreen.centery != sprite.rect.centery:
-            sprite.dy = -sprite.dy * sprite.bounce
-        sprite.rect = inscreen
-        return False
-    return True
+    if inscreen.centerx != sprite.rect.centerx:
+        sprite.dx = -sprite.dx * sprite.bounce
+    if inscreen.centery != sprite.rect.centery:
+        sprite.dy = -sprite.dy * sprite.bounce
+    sprite.rect = inscreen
 
 
 def resolve_collision(sprite, collided):
-    if sprite is not collided:  # Do not include self
-        overlap = collided.rect.clip(sprite.rect)
-        bounce = (sprite.bounce + collided.bounce) / 2
-        if abs(sprite.dx - collided.dx) > abs(sprite.dy - collided.dy):
-            # Horizontal collision
-            mag = overlap.width / 2
-            screen_move(sprite.rect, mag if sprite.rect.centerx > collided.rect.centerx else -mag, 0)
-            screen_move(collided.rect, -mag if sprite.rect.centerx > collided.rect.centerx else mag, 0)
-            sprite.dx = -collided.dx * bounce
-            collided.dx = -sprite.dx * bounce
-        else:
-            mag = overlap.height / 2
-            screen_move(sprite.rect, 0, mag if sprite.rect.centery > collided.rect.centery else -mag)
-            screen_move(collided.rect, 0, -mag if sprite.rect.centery > collided.rect.centery else mag)
-            sprite.dy = -collided.dy * bounce
-            collided.dy = -sprite.dy * bounce
+    overlap = collided.rect.clip(sprite.rect)
+    bounce = (sprite.bounce + collided.bounce) / 2
+    if abs(sprite.dx - collided.dx) > abs(sprite.dy - collided.dy):
+        # Horizontal collision
+        mag = math.ceil(overlap.width / 2)
+        screen_move(sprite.rect, mag if sprite.rect.centerx > collided.rect.centerx else -mag, 0)
+        screen_move(collided.rect, -mag if sprite.rect.centerx > collided.rect.centerx else mag, 0)
+        sprite.dx = -collided.dx * bounce
+        collided.dx = -sprite.dx * bounce
+    else:
+        mag = math.ceil(overlap.height / 2)
+        screen_move(sprite.rect, 0, mag if sprite.rect.centery > collided.rect.centery else -mag)
+        screen_move(collided.rect, 0, -mag if sprite.rect.centery > collided.rect.centery else mag)
+        sprite.dy = -collided.dy * bounce
+        collided.dy = -sprite.dy * bounce
 
 
 def screen_move(rect, x, y):
