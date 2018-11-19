d3f783e1db195315ab4d49e81a0652c58b253d09
gsvarovsky <gsvarovsky@idbs.com>
2018-11-19 12:51:30 +0000

    Switched to use a python base class for sprites

--- setup.py
+++ setup.py
@@ -9,12 +9,21 @@ WIN_STYLE = 0  # |FULLSCREEN
 
 AIR_RESISTANCE = 0.9
 
-CLASS_DEFAULTS = {
-    'mass': 10.0,
-    'elasticity': 0.3,
-    'buoyancy': 0.0,
-    'on_key': lambda *args: None
-}
+
+class Physical(pygame.sprite.Sprite):
+    mass = 10.0
+    elasticity = 0.3
+    buoyancy = 0.0
+
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.dx = self.dy = 0.0
+        self.carrying = []
+
+    # noinspection PyStatementEffect,PyUnusedLocal,PyMethodMayBeStatic
+    def on_key(self, *args):
+        None
+
 
 pygame.init()
 # Set the display mode
@@ -26,8 +35,7 @@ background = pygame.Surface(SCREEN_RECT.size)
 
 object_names = set([fn.rsplit('.', 1)[0] for fn in listdir('objects')])
 object_types = {
-    name: type(name, (pygame.sprite.Sprite,), {
-        **CLASS_DEFAULTS,
+    name: type(name, (Physical,), {
         **({'image': pygame.image.load(bmp).convert()} if path.exists(bmp) else {}),
         **({k: v for k, v in importlib.import_module(f"objects.{name}").__dict__.items() if not k.startswith('__')}
            if path.exists(f"objects/{name}.py") else {})
@@ -50,5 +58,3 @@ def put(x, y, object_type_name, object_name=None):
     sprite.rect = sprite.image.get_rect(topleft=(x, y))
     sprite.name = object_name if object_name is not None else \
         object_type_name + str(sum([1 for s in all_group.sprites() if type(s).__name__ == object_type_name]))
-    # Set default values
-    sprite.dx = sprite.dy = 0.0
